12a6f96824dd8b438c2b5ffeb5f2f1954a447162
Saurav Sarkar <67979690+saurav-sarkar@users.noreply.github.com>
2023-10-08 12:48:04 +0530

    Optimized

--- src/components/About.js
+++ src/components/About.js
@@ -17,36 +17,49 @@
     <div className="accordion-item">
         <h2 className="accordion-header" id="headingOne">
         <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Accordion Item #1
+            Why Textutil?:
         </button>
         </h2>
         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
         <div className="accordion-body" style={myStyle}>
-            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            <strong>Textutil is designed to simplify your text-related tasks, whether you're creating content, editing documents, or working on a coding project. With a user-friendly interface and a suite of powerful tools, you can streamline your work, save time, and ensure your text is error-free .</strong>
         </div>
         </div>
     </div>
     <div className="accordion-item">
         <h2 className="accordion-header" id="headingTwo">
         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Accordion Item #2
+        Who Can Benefit:
         </button>
         </h2>
         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
         <div className="accordion-body" style={myStyle}>
-            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            <strong>Writers: Enhance your writing with proper formatting, and quickly analyze your text for improvements.
+
+            Students: Count words in essays, format reports, or generate placeholder text for school projects.
+            </strong>
         </div>
         </div>
     </div>
     <div className="accordion-item">
         <h2 className="accordion-header" id="headingThree">
         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Accordion Item #3
+            Key Features:
         </button>
         </h2>
         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
         <div className="accordion-body" style={myStyle}>
-            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            <strong>Text Formatting: Effortlessly format your text with options to bold, italicize, underline, and create bulleted or numbered lists. Say goodbye to manual formatting headaches! <br /> <br />
+
+            Character and Word Count: Quickly determine the number of characters and words in your text to meet precise word count requirements. <br /> <br />
+
+            Case Conversion: Easily convert text to uppercase, lowercase, sentence case, or title case with a single click. <br /> <br />
+
+            Find and Replace: Search for specific words or phrases in your text and replace them as needed. Perfect for editing and proofreading tasks. <br /> <br />
+
+            Lorem Ipsum Generator: Generate placeholder text for your design projects in various lengths and formats, making layout design a breeze. <br /> <br />
+
+            Text Sorting: Organize lists, paragraphs, or data in alphabetical or numerical order with our intuitive sorting tool..</strong> 
         </div>
         </div>
     </div>
